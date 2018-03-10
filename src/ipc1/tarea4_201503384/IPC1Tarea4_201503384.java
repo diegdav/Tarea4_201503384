506b522c56c6663a944761a313974e40bb2ac591
diego <diego@DESKTOP-J4RC1E8>
2018-03-10 12:10:59 -0600

    Tarea4 inicializada

---
+++ src/ipc1/tarea4_201503384/IPC1Tarea4_201503384.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.tarea4_201503384;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author diego
+ */
+public class IPC1Tarea4_201503384 extends JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
