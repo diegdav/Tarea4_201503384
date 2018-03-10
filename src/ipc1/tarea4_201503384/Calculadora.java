53fe4102a9f6602eeeea0f95ef94ae111f7c458b
diego <diego@DESKTOP-J4RC1E8>
2018-03-10 12:29:07 -0600

    proceso

---
+++ src/ipc1/tarea4_201503384/Calculadora.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.tarea4_201503384;
+
+import java.awt.HeadlessException;
+import javax.swing.*;
+
+/**
+ *
+ * @author diego
+ */
+public class Calculadora extends JFrame {
+
+    public Calculadora() {
+        setTitle("Calculadora");
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        
+        Panel panel = new Panel();
+        panel.setVisible(true);
+        add(panel);
+    }
+}
