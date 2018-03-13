2d205054f85d221c6b0a2b1440ea0085fa77f136
diego <diego@DESKTOP-J4RC1E8>
2018-03-12 22:25:59 -0600

    Tarea4 Finalizada

--- src/ipc1/tarea4_201503384/IPC1Tarea4_201503384.java
+++ src/ipc1/tarea4_201503384/IPC1Tarea4_201503384.java
@@ -17,6 +17,17 @@ public class IPC1Tarea4_201503384 extends JFrame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Calculadora.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
         Calculadora verCalculadora = new Calculadora();
         verCalculadora.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         verCalculadora.setVisible(true);
